eeb08277304a3b81fce62cb125be757d5bc52e0e
Fernando <fp.naldo@gmail.com>
2021-04-26 19:55:59 -0400

    Created 2000 sales for the coffee graphs with the Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+2000.times do
+    Sale.create(coffee: Faker::Coffee.blend_name, origin: Faker::Coffee.origin, variety: Faker::Coffee.variety, amount: rand(1990..5490), date_time: Faker::Date.between(from: '2017-01-01', to: Date.today))
+end
